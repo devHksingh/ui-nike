48c6e6b61eca2c414f31f2faccaa55f1fd12166a
devHksingh <hksingh.dev@gmail.com>
2024-11-04 12:08:18 +0530

    setup comp

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,13 @@
-
+import {
+  CustomerReviews,
+  Footer,
+  Hero,
+  PopularProducts,
+  Services,
+  SpecialOffer,
+  Subscribe,
+  SuperQuality
+} from "./sections"
 import './App.css'
 
 function App() {
@@ -6,9 +15,33 @@ function App() {
 
   return (
     <>
-      <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+      <main className='relative'>
+        nav
+        <section className='xl:padding-1 wide:padding-r padding-b'>
+          <Hero/>
+        </section>
+        <section className='padding'>
+          <PopularProducts/>
+        </section>
+        <section className='padding'>
+          <SuperQuality/>
+        </section>
+        <section className='py-10 padding-x'>
+          <Services/>
+        </section>
+        <section className='padding'>
+          <SpecialOffer/>
+        </section>
+        <section className='padding bg-pale-blue '>
+          <CustomerReviews/>
+        </section>
+        <section className='w-full py-16 padding-x sm:py-32'>
+          <Subscribe/>
+        </section>
+        <section className='pb-8 bg-black padding-x padding-t'>
+          <Footer/>
+        </section>
+      </main>
     </>
   )
 }
