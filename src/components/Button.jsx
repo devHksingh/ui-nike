fcccd1b3f9ff9745405f8916bbfa8044342b5a5d
devHksingh <hksingh.dev@gmail.com>
2024-11-07 08:32:37 +0530

    fix icon issue

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -4,9 +4,9 @@ const Button = ({label,iconUrl}) => {
   return (
     <button className="flex items-center justify-center gap-2 py-4 text-lg leading-none text-white border rounded-full px-7 font-montserrat bg-coral-red border-coral-red hover:bg-orange-800">
       {label} 
-      <img src={iconUrl} alt="icon"
+      { iconUrl && <img src={iconUrl} alt="icon"
       className="w-5 h-5 ml-2 rounded-full"
-      />
+      />}
 
     </button>
   )
